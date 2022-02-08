3e9a2cd433edf9f7c6de7284b44785c61b3a5504
escovask8 <domagal@gmail.com>
2022-02-08 17:07:27 -0300

    chore: inserindo uma nova categoria com o post

--- src/main/java/com/victor/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/victor/cursomc/resources/CategoriaResource.java
@@ -3,12 +3,16 @@ package com.victor.cursomc.resources;
 
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.victor.cursomc.domain.Categoria;
 import com.victor.cursomc.services.CategoriaService;
@@ -24,7 +28,16 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
-		
 	}
-}	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert( @RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+}
+
+
