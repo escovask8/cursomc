cf8837e4d5c6f5e7a45b0e371f5dcb9736d31bcb
escovask8 <domagal@gmail.com>
2022-02-08 17:33:05 -0300

    chore: atualizando categoria com put e outros ajustes

--- src/main/java/com/victor/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/victor/cursomc/resources/CategoriaResource.java
@@ -25,8 +25,8 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
@@ -37,6 +37,13 @@ public class CategoriaResource {
 			.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)	
+		public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+			obj.setId(id);
+			obj = service.update(obj);
+			return ResponseEntity.noContent().build();
+		}
 
 }
 
--- src/main/java/com/victor/cursomc/services/CategoriaService.java
+++ src/main/java/com/victor/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
@@ -25,6 +25,10 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	
-}
+	}
 	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 }
