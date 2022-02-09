f5d3ba2a9742003bc68f00ffe90d0ce70684f10b
escovask8 <domagal@gmail.com>
2022-02-09 10:25:23 -0300

    chore: listando todas categorias com DTO

--- src/main/java/com/victor/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/victor/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ package com.victor.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.victor.cursomc.domain.Categoria;
+import com.victor.cursomc.dto.CategoriaDTO;
 import com.victor.cursomc.services.CategoriaService;
 
 @RestController
@@ -51,6 +54,13 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 		
 		
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());  
+		return ResponseEntity.ok().body(listDto);
 	}
 }
 
--- src/main/java/com/victor/cursomc/services/CategoriaService.java
+++ src/main/java/com/victor/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.victor.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,8 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
