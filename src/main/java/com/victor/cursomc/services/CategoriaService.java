7ff1289852277092d44fb4206372676601d73095
escovask8 <domagal@gmail.com>
2022-02-09 15:19:47 -0300

    chore: Bean Validation

--- src/main/java/com/victor/cursomc/services/CategoriaService.java
+++ src/main/java/com/victor/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.victor.cursomc.domain.Categoria;
+import com.victor.cursomc.dto.CategoriaDTO;
 import com.victor.cursomc.repositories.CategoriaRepository;
 import com.victor.cursomc.services.exceptions.DataIntegrityException;
 import com.victor.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,5 +53,8 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
