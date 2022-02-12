9b1becee19eae0e6ad94d244907928fe6d01c4f3
Victor Dutra <domagal@gmail.com>
2022-02-12 18:33:10 -0300

    update correção em CategoriaService e construtor de Pagamento

--- src/main/java/com/victor/cursomc/services/CategoriaService.java
+++ src/main/java/com/victor/cursomc/services/CategoriaService.java
@@ -35,8 +35,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	public void delete(Integer id) {
 		find(id);
@@ -56,5 +57,8 @@ public class CategoriaService {
 	}
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
