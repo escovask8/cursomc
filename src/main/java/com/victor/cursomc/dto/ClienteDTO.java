6ba2c03e17ea6ac169ced78b1a754bbd79c708f9
Victor Dutra <domagal@gmail.com>
2022-02-12 18:20:05 -0300

    chore: PUT, DELETE e GET para cliente

---
+++ src/main/java/com/victor/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package com.victor.cursomc.dto;
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import com.victor.cursomc.domain.Cliente;
+
+import org.hibernate.validator.constraints.Length;
+
+public class ClienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+   
+    private Integer id;
+    @NotEmpty(message="Preenchimento obrigatório")
+    @Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+    
+    private String nome;
+
+    @NotEmpty(message="Preencimento obrigatório")
+    @Email(message="Email inválido")
+    private String email;
+    
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Cliente obj) {
+        id = obj.getId();
+        nome = obj.getNome();
+        email = obj.getEmail();
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
+    
+
+
