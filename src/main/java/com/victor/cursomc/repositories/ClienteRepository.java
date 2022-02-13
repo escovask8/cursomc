d1e00c450d82840d8dd28331d7f859bda92a3109
Victor Dutra <domagal@gmail.com>
2022-02-12 22:41:17 -0300

    chore:validação para email repetido

--- src/main/java/com/victor/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/victor/cursomc/repositories/ClienteRepository.java
@@ -1,13 +1,16 @@
 package com.victor.cursomc.repositories;
+import com.victor.cursomc.domain.Cliente;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.victor.cursomc.domain.Cliente;
+import org.springframework.transaction.annotation.Transactional;
 
 
 
 @Repository
 public interface ClienteRepository extends JpaRepository  <Cliente, Integer>{
 
+    @Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/victor/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/victor/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.victor.cursomc.domain.Cliente;
 import com.victor.cursomc.domain.enums.TipoCliente;
 import com.victor.cursomc.dto.ClienteNewDTO;
+import com.victor.cursomc.repositories.ClienteRepository;
 import com.victor.cursomc.resources.exception.FieldMessage;
 import com.victor.cursomc.services.validation.utils.BR;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -24,6 +31,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
