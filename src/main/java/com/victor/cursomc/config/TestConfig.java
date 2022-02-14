d7e71c89589eaa54060315fad87564a48284ec26
escovask8 <domagal@gmail.com>
2022-02-14 09:36:11 -0300

    chore: Criando profile de teste

---
+++ src/main/java/com/victor/cursomc/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.victor.cursomc.config;
+
+import java.text.ParseException;
+
+import com.victor.cursomc.services.DBService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+}
