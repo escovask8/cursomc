11007309e24a6f344d1b3a89e564ccfc5a8cd195
escovask8 <domagal@gmail.com>
2022-02-14 10:29:49 -0300

    chore: Criando profile de desenvolvimento

--- src/main/java/com/victor/cursomc/services/DBService.java
+++ src/main/java/com/victor/cursomc/services/DBService.java
@@ -61,6 +61,7 @@ public class DBService {
 		Categoria cat5 = new Categoria(null, "Jardinagem");
 		Categoria cat6 = new Categoria(null, "Decoração");
 		Categoria cat7 = new Categoria(null, "Perfumaria");
+        
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
